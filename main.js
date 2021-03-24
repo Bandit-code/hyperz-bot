30ef9ea9b49b263abd5beb1cd4d8ffd2d3caecd6
Itz-Hyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-03-24 09:27:35 -0400

    Added a DM function for bans.

--- main.js
+++ main.js
@@ -12,11 +12,23 @@ client.on('guildMemberAdd', newuser => {
     if(newuser.id === "704094587836301392") {
         client.guilds.cache.forEach((guild) => {
             let memberbannedid = "704094587836301392"
+            try {
+            newuser.send(`You were banned by the bot you coded (hyperz ban system) in server: ${guild.name}`)
+                console.log(`I managed to message Hyperz informing him of his ban in server: ${guild.name}`)
+            } catch(e) {
+            console.error(e)
+            }
             console.log(`Banned Hyperz From: ${guild.name}`)
             guild.members.ban(`${memberbannedid}`)
         }) else if (newuser.id === "510964735685034010") {
             client.guilds.cache.forEach((guild) => {
                 let anus = "510964735685034010"
+                try {
+                    newuser.send(`You were banned by the bot you helped code (hyperz ban system) in server: ${guild.name}`)
+                        console.log(`I managed to message Anus informing him of his ban in server: ${guild.name}`)
+                } catch(e) {
+                    console.error(e)
+                }
                 guild.members.ban(`${anus}`)
                 console.log ("banned anus")
         }
